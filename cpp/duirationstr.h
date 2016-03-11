984b0cf8eef1f99f23dfbe6c3b0f763024e3df5e
郭荣飞 <gurongfei@126.com>
2016-03-11 10:30:10 +0800

    添加用于打印 std::chrono::duration 的相关函数。

---
+++ cpp/duirationstr.h
@@ -0,0 +1,43 @@
+#ifndef DUIRATIONSTR_H_M1VNJOF5
+#define DUIRATIONSTR_H_M1VNJOF5
+
+#include <string>
+#include <chrono>
+
+using std::string;
+using std::chrono::duration;
+
+namespace XDUtil {
+
+/**
+ * @brief  convert duration to stream.
+ *
+ * @tparam V
+ * @tparam R
+ * @param d the duration to convert.
+ *
+ * @returns   the string represent of the duration
+ */
+template <typename V, typename R>
+std::string DurationStr (const std::chrono::duration<V, R>& d) {
+    return std::string("["
+                       + std::to_string(d.count())
+                       + " of "
+                       + std::to_string(R::num)
+                       + std::to_string(R::den)
+                       + "]");
+}
+
+/**
+ * @brief  print the duration to ostream
+ */
+template <typename V, typename R>
+std::ostream& operator << (std::ostream&s,
+                           const std::chrono::duration<V, R>& d) {
+    return s << DurationStr(d);
+}
+
+
+} // XDUtil
+
+#endif /* end of include guard: DUIRATIONSTR_H_M1VNJOF5 */
