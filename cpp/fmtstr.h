4e9bf09ba367bc792ec6839fe1ee81447059e607
郭荣飞 <gurongfei@126.com>
2016-04-24 21:04:21 +0800

    把 fmtstr 放入到 XDUtil 命名空间中

--- cpp/fmtstr.h
+++ cpp/fmtstr.h
@@ -2,6 +2,10 @@
 #define CPP_UTILITIES_FMTSTR_H
 
 #include <string>
+#include <sstream>
+
+namespace XDUtil {
+
 
 /**
  * @brief  A python style string format function. i.e.
@@ -42,6 +46,41 @@ template <typename... Types>
 const std::string FormatString(const std::string& fmt_spec,
                                const Types&... args);
 
-#include "fmtstr.cpp"
+
+void BuildFormatString(std::ostringstream& builder,
+                              const std::string& fmt_spec,
+                              std::string::size_type idx) {
+    auto count = fmt_spec.size() - idx;
+    if (count <= 0) {
+        return;
+    }
+
+    builder.write(fmt_spec.data() + idx, count);
+}
+
+template <typename T, typename... Types>
+void BuildFormatString(std::ostringstream& builder, const std::string& fmt_spec,
+                       std::string::size_type idx, const T& first,
+                       const Types&... args) {
+    auto pos = fmt_spec.find_first_of("{}", idx);
+    if (pos == std::string::npos) {
+        return BuildFormatString(builder, fmt_spec, idx);
+    }
+
+    builder.write(fmt_spec.data() + idx, pos - idx);
+    builder << first;
+    BuildFormatString(builder, fmt_spec, pos + 2, args...);
+}
+
+template <typename... Types>
+const std::string FormatString(const std::string& fmt_spec,
+                               const Types&... args) {
+    std::ostringstream builder;
+    BuildFormatString(builder, fmt_spec, 0, args...);
+    return builder.str();
+}
+
+
+} // XDUtil
 
 #endif /* end of include guard: CPP_UTILITIES_FMTSTR_H_ */
